16a504c5b53d410e86e83672c7dd0bdf0bf0db3a
Sergii79 <110824315+Sergii79@users.noreply.github.com>
2023-11-23 14:40:03 +0000

    Deploying to gh-pages from @ Sergii79/training-on-js@20be6439592a964c8e9a0e7faabd9f2409a94065 🚀

---
+++ сomponents/02-module_1-function.171cb7e3.js
@@ -0,0 +1,2 @@
+console.log("Lesson 2");"ECMAScript"===prompt("Яка офіційна назва JavaScript?")?alert("Правильно"):alert("Не знаєте? ECMAScript!");
+//# sourceMappingURL=02-module_1-function.171cb7e3.js.map
---
+++ сomponents/02-module_1-function.cea62de6.js
@@ -0,0 +1,2 @@
+console.log("Lesson 2"),"ECMAScript"===prompt("Яка офіційна назва JavaScript?")?alert("Правильно"):alert("Не знаєте? ECMAScript!");
+//# sourceMappingURL=02-module_1-function.cea62de6.js.map
