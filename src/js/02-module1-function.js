32bc0e47c2bb135d8b265ae08ef57a0cdbb7a67d
Sergii79 <tsymbal.sergiy@gmail.com>
2023-11-24 20:06:06 +0100

    first commit

--- src/js/02-module1-function.js
+++ src/js/02-module1-function.js
@@ -327,3 +327,27 @@ console.log('-----');
 // if (!loginName) {
 //   console.log('Скасовано');
 // }
+
+// function getSubscriptionPrice(type) {
+//   let price;
+//   // Change code below this line
+
+//   switch (
+//     type // Change this line
+//   ) {
+//     case 'professional': // Change this line
+//       price = 20; // Change this line
+//       break;
+
+//     case 'organization': // Change this line
+//       price = 50; // Change this line
+//       break;
+
+//     case 'starter': // Change this line
+//       price = 0; // Change this line
+//       break;
+//   }
+
+//   // Change code above this line
+//   return price;
+// }
