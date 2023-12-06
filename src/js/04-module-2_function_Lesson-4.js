8d9035d2e9d3a8f54c7dee7ced8813dfa7c07ba7
Sergii79 <tsymbal.sergiy@gmail.com>
2023-12-06 21:31:01 +0100

    first commit

--- src/js/04-module-2_function_Lesson-4.js
+++ src/js/04-module-2_function_Lesson-4.js
@@ -333,3 +333,13 @@ pricePerWord = 2;
 
 const totalPrice = calculateEngravingPrice(message, pricePerWord);
 console.log(`Total price: ${totalPrice}`);
+
+function calculateTotalPrice(order) {
+  let total = 0;
+  // Change code below this line
+  for (let i = 0; i < order.length; i++) {
+    total += order[i];
+  }
+  // Change code above this line
+  return total;
+}
