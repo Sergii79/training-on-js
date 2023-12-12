027969880813e333039984b63c8c085690fa5466
Sergii79 <tsymbal.sergiy@gmail.com>
2023-12-12 07:41:07 +0100

    commit fourth

--- src/js/05-module-3_object_Lesson-5.js
+++ src/js/05-module-3_object_Lesson-5.js
@@ -15,3 +15,37 @@ for (const key of keys) {
   // Значення властивості
   console.log(`This ${book[key]}`);
 }
+
+const temps = [14, -4, 25, 8, 11];
+
+// В консолі буде масив
+console.log(temps);
+// ❌ Так не спрацює, тому що передаємо цілий масив
+console.log(Math.max(temps)); // NaN
+
+// В консолі буде набір окремих чисел
+console.log(...temps);
+// ✅ Розподілимо колекцію елементів у якості окремих аргументів
+console.log(Math.max(...temps)); // 25
+
+console.log('----');
+
+// function multiply(...args) {
+//   console.log(args); // масив усіх аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+console.log('----');
+
+function multiply(firstNumber, secondNumber, ...otherArgs) {
+  console.log(firstNumber); // Значення першого аргументу
+  console.log(secondNumber); // Значення другого аргументу
+  console.log(otherArgs); // Масив інших аргументів
+}
+
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
