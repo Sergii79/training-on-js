43038fd6e1fd58b8ae95b6f7091445cd88047f83
Sergii79 <110824315+Sergii79@users.noreply.github.com>
2023-12-13 08:24:13 +0000

    Deploying to gh-pages from @ Sergii79/training-on-js@47cb8176dbde47238dc051aaed0828d806c212ef 🚀

--- сomponents/06-module-3_object_Lesson-6.38aea34f.js
+++
@@ -1,2 +0,0 @@
-console.log("Модуль 3 Заняття 6. Деструктуризація та rest/spread");const e=function({firstName:e,lastName:o,friends:s,...n}){return{...n,fullName:`${e} ${o}`}}({id:1,firstName:"Jacob",lastName:"Mercer",email:"j.mercer@mail.com",friendCount:40,friends:{Anton:"best",Ivan:"so so"}});console.log(e);
-//# sourceMappingURL=06-module-3_object_Lesson-6.38aea34f.js.map
