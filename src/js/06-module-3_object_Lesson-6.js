dba9d0c9ba7c2497b6c6af5d632c4f03c9ec5be6
Sergii79 <tsymbal.sergiy@gmail.com>
2023-12-18 08:00:28 +0100

    commit second

--- src/js/06-module-3_object_Lesson-6.js
+++ src/js/06-module-3_object_Lesson-6.js
@@ -403,7 +403,11 @@ console.log(getAllPropValues('category'));
 
 console.log('------');
 
-const scores = [89, 64, 42, 17, 93, 51, 26];
 // Change code below this line
-const bestScore = Math.max(...scores);
-const worstScore = Math.min(...scores);
+function add(...args) {
+  // Change code above this line
+
+  let sum = 0;
+  for (i of args) sum += i;
+  return sum;
+}
