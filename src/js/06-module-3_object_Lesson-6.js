1b36290e14cdc9121c12d7070ceffea4c5bb1085
Sergii79 <tsymbal.sergiy@gmail.com>
2023-12-19 16:47:37 +0100

    commit second

--- src/js/06-module-3_object_Lesson-6.js
+++ src/js/06-module-3_object_Lesson-6.js
@@ -403,12 +403,12 @@ console.log(getAllPropValues('category'));
 
 console.log('------');
 
-const bookShelf = {
-  books: ['The last kingdom', 'Haze', 'The guardian of dreams'],
-  updateBook(oldName, newName) {
-    // Change code below this line
-    const bookIndex = this.books.indexOf(oldName);
-    this.books.splice(bookIndex, 1, newName);
-    // Change code above this line
+const atTheOldToad = {
+  potions: ['Speed potion', 'Dragon breath', 'Stone skin'],
+  // Change code below this line
+
+  getPotions() {
+    return this.potions;
   },
+  // Change code above this line
 };
