ffc729a677d584bf55ba5a1d0557a9bd1b58eeb9
Sergii79 <tsymbal.sergiy@gmail.com>
2023-12-27 08:41:58 +0100

    commit second

--- src/js/07-module-4_iterative_methods_lesson-7.js
+++ src/js/07-module-4_iterative_methods_lesson-7.js
@@ -177,45 +177,47 @@ const newNumbers = each(numbers, function (value) {
 
 console.log(newNumbers);
 
-// console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
-//     return value * 2;
-//   }),
-// );
-// console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
-//     return value - 10;
-//   }),
-// );
-// console.log(
-//   each([64, 49, 36, 25, 16], function (value) {
-//     return Math.sqrt(value);
-//   }),
-// );
-// console.log(
-//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
-//     return Math.ceil(value);
-//   }),
-// );
-// console.log(
-//   each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
-//     return Math.floor(value);
-//   }),
-// );
+console.log(
+  each([64, 49, 36, 25, 16], function (value) {
+    return value * 2;
+  })
+);
+console.log(
+  each([64, 49, 36, 25, 16], function (value) {
+    return value - 10;
+  })
+);
+console.log(
+  each([64, 49, 36, 25, 16], function (value) {
+    return Math.sqrt(value);
+  })
+);
+console.log(
+  each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+    return Math.ceil(value);
+  })
+);
+console.log(
+  each([1.5, 2.1, 16.4, 9.7, 11.3], function (value) {
+    return Math.floor(value);
+  })
+);
 
-// const users = [{
-//   name: "artem",
-//   age: 30
-// }];
+const users = [
+  {
+    name: 'artem',
+    age: 30,
+  },
+];
 
-// const res = each(users, function (user) {
-//   return {
-//     ...user,
-//     name: user.name.toUpperCase()
-//   }
-// });
+const res = each(users, function (user) {
+  return {
+    ...user,
+    name: user.name.toUpperCase(),
+  };
+});
 
-// console.log(res)
+console.log(res);
 
 // ```
 
