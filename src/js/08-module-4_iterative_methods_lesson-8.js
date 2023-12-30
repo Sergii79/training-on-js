f47e7fadbf6e98ee92d7515bc61764ee02a5f45c
Sergii79 <tsymbal.sergiy@gmail.com>
2023-12-30 09:20:50 +0100

    commit second

--- src/js/08-module-4_iterative_methods_lesson-8.js
+++ src/js/08-module-4_iterative_methods_lesson-8.js
@@ -478,16 +478,26 @@ console.log(array[2]);
 
 console.log('------');
 
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+];
 // Change code below this line
-const getCommonElements = (firstArray, secondArray) => {
-  const commonElements = [];
 
-  firstArray.forEach(element => {
-    if (secondArray.includes(element)) {
-      commonElements.push(element);
-    }
-  });
-
-  // Change code above this line
-  return commonElements;
-};
+const titles = books.map(book => book.title);
+console.log(titles);
