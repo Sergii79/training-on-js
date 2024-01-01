8f8c1f861e5c431473378933cec08303ff3df97b
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-01 16:08:15 +0100

    commit second

--- src/js/08-module-4_iterative_methods_lesson-8.js
+++ src/js/08-module-4_iterative_methods_lesson-8.js
@@ -477,28 +477,3 @@ const array = ['Jacob', 'William', 'Solomon', 'Artemis'];
 console.log(array[2]);
 
 console.log('------');
-
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    rating: 8.51,
-  },
-  {
-    title: 'The Dream of a Ridiculous Man',
-    author: 'Fyodor Dostoevsky',
-    rating: 7.75,
-  },
-  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-];
-const BOOK_TITLE = 'The Dream of a Ridiculous Man';
-const AUTHOR = 'Robert Sheckley';
-// Change code below this line
-
-const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
-const bookByAuthor = books.find(book => book.author === AUTHOR);
