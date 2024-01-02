b0ada668a60bd973ca4f9a115ca5c89e01646b98
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-02 08:42:05 +0100

    commit second

--- src/js/08-module-4_iterative_methods_lesson-8.js
+++ src/js/08-module-4_iterative_methods_lesson-8.js
@@ -478,14 +478,40 @@ console.log(array[2]);
 
 console.log('------');
 
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+];
 // Change code below this line
-const getTotalFriendCount = users => {
-  return users.reduce((total, friend) => total + friend.friends.length, 0);
-};
-// Change code above this line
 
-// const getTotalFriendCount = users => {
-//       return users.reduce((acc, user)=>{
-//      return acc + user.friends.length
-//   },0)
-// };
+const sortedByAuthorName = [...books].sort((firstAuthor, secondAuthor) =>
+  firstAuthor.author.localeCompare(secondAuthor.author)
+);
+
+const sortedByReversedAuthorName = [...books].sort(
+  (firstAuthor, secondAuthor) =>
+    secondAuthor.author.localeCompare(firstAuthor.author)
+);
+
+const sortedByAscendingRating = [...books].sort(
+  (firstRating, secondRating) => firstRating.rating - secondRating.rating
+);
+
+const sortedByDescentingRating = [...books].sort(
+  (firstRating, secondRating) => secondRating.rating - firstRating.rating
+);
