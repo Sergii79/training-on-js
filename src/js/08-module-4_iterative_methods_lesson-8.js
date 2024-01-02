d036351b6e634e38571876140a615dc3fd4edb12
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-02 20:06:22 +0100

    commit second

--- src/js/08-module-4_iterative_methods_lesson-8.js
+++ src/js/08-module-4_iterative_methods_lesson-8.js
@@ -478,40 +478,9 @@ console.log(array[2]);
 
 console.log('------');
 
-const books = [
-  {
-    title: 'The Last Kingdom',
-    author: 'Bernard Cornwell',
-    rating: 8.38,
-  },
-  {
-    title: 'Beside Still Waters',
-    author: 'Robert Sheckley',
-    rating: 8.51,
-  },
-  {
-    title: 'The Dream of a Ridiculous Man',
-    author: 'Fyodor Dostoevsky',
-    rating: 7.75,
-  },
-  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
-  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
-];
 // Change code below this line
-
-const sortedByAuthorName = [...books].sort((firstAuthor, secondAuthor) =>
-  firstAuthor.author.localeCompare(secondAuthor.author)
-);
-
-const sortedByReversedAuthorName = [...books].sort(
-  (firstAuthor, secondAuthor) =>
-    secondAuthor.author.localeCompare(firstAuthor.author)
-);
-
-const sortedByAscendingRating = [...books].sort(
-  (firstRating, secondRating) => firstRating.rating - secondRating.rating
-);
-
-const sortedByDescentingRating = [...books].sort(
-  (firstRating, secondRating) => secondRating.rating - firstRating.rating
-);
+const getTotalBalanceByGender = (users, gender) => {
+  return [...users]
+    .filter(item => item.gender === gender)
+    .reduce((previousValue, number) => (previousValue += number.balance), 0);
+};
