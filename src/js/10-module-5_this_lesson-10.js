84f8fb43a3be527e4a4b5e54f56c0edc0c3bd37b
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-12 22:44:22 +0100

    commit second

--- src/js/10-module-5_this_lesson-10.js
+++ src/js/10-module-5_this_lesson-10.js
@@ -319,20 +319,20 @@ console.groupEnd('secondToggle');
 
 console.log('---Example---');
 
-// const pizzaPalace = {
-//   pizzas: ['Supercheese', 'Smoked', 'Four meats'],
-//   // Change code below this line
-//   checkPizza(pizzaName) {
-//     return this.pizzas.includes(pizzaName);
-//   },
-//   order(pizzaName) {
-//     const isPizzaAvailable = this.checkPizza(pizzaName);
+const pizzaPalace = {
+  pizzas: ['Supercheese', 'Smoked', 'Four meats'],
+  // Change code below this line
+  checkPizza(pizzaName) {
+    return this.pizzas.includes(pizzaName);
+  },
+  order(pizzaName) {
+    const isPizzaAvailable = this.checkPizza(pizzaName);
 
-//     if (!isPizzaAvailable) {
-//       return `Sorry, there is no pizza named «${pizzaName}»`;
-//     }
+    if (!isPizzaAvailable) {
+      return `Sorry, there is no pizza named «${pizzaName}»`;
+    }
 
-//     return `Order accepted, preparing «${pizzaName}» pizza`;
-//   },
-//   // Change code above this line
-// };
+    return `Order accepted, preparing «${pizzaName}» pizza`;
+  },
+  // Change code above this line
+};
