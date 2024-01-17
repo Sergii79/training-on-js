58a43f6b1c4ae531026f8e870e72f89b3d80895d
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-17 09:27:39 +0100

    commit second

--- src/js/10-module-5_this_lesson-10.js
+++ src/js/10-module-5_this_lesson-10.js
@@ -407,34 +407,33 @@ console.log(storageNew.getItems()); // ["Nanitoids", "Prolonger", "Antigravitato
 storageNew.removeItem('Prolonger');
 console.log(storageNew.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-class StringBuilder {
-  constructor(initialValue) {
-    this.value = initialValue;
+class Car {
+  // Change code below this line
+  static MAX_PRICE = 50000;
+  #price;
+
+  constructor({ price }) {
+    this.#price = price;
   }
 
-  getValue() {
-    return this.value;
+  get price() {
+    return this.#price;
   }
 
-  padEnd(str) {
-    this.value += str;
+  set price(newPrice) {
+    if (newPrice > Car.MAX_PRICE) {
+      return;
+    }
+    this.#price = newPrice;
   }
-
-  padStart(str) {
-    this.value = str + this.value;
-  }
-
-  padBoth(str) {
-    this.value = str + this.value + str;
-  }
+  // Change code above this line
 }
 
-// Change code above this line
-const builder = new StringBuilder('.');
-console.log(builder.getValue()); // "."
-builder.padStart('^');
-console.log(builder.getValue()); // "^."
-builder.padEnd('^');
-console.log(builder.getValue()); // "^.^"
-builder.padBoth('=');
-console.log(builder.getValue()); // "=^.^="
+const audi = new Car({ price: 35000 });
+console.log(audi.price); // 35000
+
+audi.price = 49000;
+console.log(audi.price); // 49000
+
+audi.price = 51000;
+console.log(audi.price); // 49000
