c8315c7b59ffa6fe514b5e2c243955fba08c2d29
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-18 07:17:59 +0100

    commit second

--- src/js/10-module-5_this_lesson-10.js
+++ src/js/10-module-5_this_lesson-10.js
@@ -375,6 +375,8 @@ storageNew.removeItem('Prolonger');
 console.log(storageNew.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
 class User {
+  email;
+
   constructor(email) {
     this.email = email;
   }
@@ -387,11 +389,40 @@ class User {
     this.email = newEmail;
   }
 }
-// Change code below this line
-
 class Admin extends User {
+  // Change code below this line
+
   static AccessLevel = {
     BASIC: 'basic',
     SUPERUSER: 'superuser',
   };
+
+  constructor({ email, accessLevel }) {
+    super(email);
+    this.accessLevel = accessLevel;
+  }
+
+  // Change code above this line
+  blacklistedEmails = [];
+
+  blacklist(email) {
+    this.blacklistedEmails.push(email);
+  }
+
+  isBlacklisted(email) {
+    return this.blacklistedEmails.includes(email);
+  }
 }
+
+const mango = new Admin({
+  email: 'mango@mail.com',
+  accessLevel: Admin.AccessLevel.SUPERUSER,
+});
+
+console.log(mango.email); // "mango@mail.com"
+console.log(mango.accessLevel); // "superuser"
+
+mango.blacklist('poly@mail.com');
+console.log(mango.blacklistedEmails); // ["poly@mail.com"]
+console.log(mango.isBlacklisted('mango@mail.com')); // false
+console.log(mango.isBlacklisted('poly@mail.com')); // true
