4d581e20954be6ad2d82df87667d0d72ff14e94d
Sergii79 <110824315+Sergii79@users.noreply.github.com>
2024-01-24 07:17:38 +0000

    Deploying to gh-pages from @ Sergii79/training-on-js@dd6b67574f3babe7d4fb00a163d4513feb5bfa7f 🚀

--- сomponents/12-module-6_dom-and-event_lesson-12.be20bdd2.js
+++
@@ -1,2 +0,0 @@
-var modalHandler=function(e){console.log("button clicked")},btnElem=document.querySelector("#myBtn");btnElem.addEventListener("click",modalHandler);
-//# sourceMappingURL=12-module-6_dom-and-event_lesson-12.be20bdd2.js.map
