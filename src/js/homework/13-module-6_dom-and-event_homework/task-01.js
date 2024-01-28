f541252423d1c16c9dbb751a7d0971bace310423
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-28 10:23:01 +0100

    commit second

--- src/js/homework/13-module-6_dom-and-event_homework/task-01.js
+++ src/js/homework/13-module-6_dom-and-event_homework/task-01.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+const liCategories = document.querySelectorAll('.item');
+console.log(liCategories);
+
+const arrayCategories = [];
+
+liCategories.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  arrayCategories.push(categoryName);
+});
+
+console.log(arrayCategories);
+console.log('Number of categories:', arrayCategories.length);
+
+const categoryTitle = arrayCategories.forEach(category =>
+  console.log('Category:', category)
+);
+
+// const catecorySubtitle =
