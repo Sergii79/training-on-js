653542f0b2cb5df95171473bb026ceaad022320a
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-29 07:30:45 +0100

    commit second

--- src/js/homework/13-module-6_dom-and-event_homework/task-01.js
+++ src/js/homework/13-module-6_dom-and-event_homework/task-01.js
@@ -1,18 +1,35 @@
-const liCategories = document.querySelectorAll('.item');
-console.log(liCategories);
+//* 1. Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+//* 2. Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
-const arrayCategories = [];
+/*
+    1. Створити константу для категорій  - li.item
+    2. Вивести кількість категорій
+    3. Створити цикл для перебoру елементів категорій
+    4. Знайти Заголовки <h2>
+    5. Вивести контент Заголовків
+    6. Знайти кількість елементів в категорії <li>
+    7. Вивести знайдену кількість елементів   
+*/
 
-liCategories.forEach(category => {
-  const categoryName = category.querySelector('h2').textContent;
-  arrayCategories.push(categoryName);
+const categories = document.querySelectorAll('.item');
+
+console.log(categories);
+console.log('Number of categories:', categories.length);
+
+//* 1-й варіант:
+
+// for (const category of categories) {
+//   const title = category.firstElementChild.textContent;
+//   console.log('Category:', title);
+//   const subtitle = category.lastElementChild.children.length;
+//   console.log('Elements:', subtitle);
+// }
+
+//* 2-й варіант:
+
+categories.forEach(item => {
+  const title = item.firstElementChild.textContent;
+  console.log('Category:', title);
+  const subtitle = item.lastElementChild.children.length;
+  console.log('Elements:', subtitle);
 });
-
-console.log(arrayCategories);
-console.log('Number of categories:', arrayCategories.length);
-
-const categoryTitle = arrayCategories.forEach(category =>
-  console.log('Category:', category)
-);
-
-// const catecorySubtitle =
--- src/js/homework/13-module-6_dom-and-event_homework/task-02.js
+++ src/js/homework/13-module-6_dom-and-event_homework/task-02.js
@@ -1,3 +1,18 @@
+//* 1. Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+//* 2. Додасть назву інгредієнта як його текстовий вміст.
+//* 3. Додасть елементу клас item.
+//* 4. Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+/*
+    1. Створити цикл для перебoру елементів категорій 
+    2. Створити елемент <li>
+    3. Додати назви в створені <li>
+    4. Створит контейнер для подальшого запису в html
+    5. Запушити елемент <li> в контейнер
+    6. Додати елементу клас item.
+    7. Вивести елемент <li> 
+*/
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -7,4 +22,39 @@ const ingredients = [
   'Condiments',
 ];
 
-console.log('Hello!');
+const container = document.getElementById('ingredients');
+
+//* 1-й варіант:
+
+// for (const item of ingredients) {
+//   const itemElement = document.createElement('li');
+//   itemElement.textContent = item;
+//   container.append(itemElement);
+//   itemElement.classList.add('item');
+
+//   console.log(itemElement);
+// }
+
+//* 2-й варіант:
+
+// const list = ingredients.map(item => {
+//   const itemElement = document.createElement('li');
+//   itemElement.textContent = item;
+//   itemElement.classList.add('item');
+//   return itemElement;
+// });
+// container.append(...list);
+// console.log(list);
+
+//* 3-й варіант:
+
+const ulElements = document.querySelector('#ingredients');
+
+ingredients.forEach(ingredientItem => {
+  const liElement = document.createElement('li');
+  liElement.textContent = ingredientItem;
+  ulElements.append(liElement);
+  liElement.classList.add('item');
+});
+
+console.log(ulElements);
