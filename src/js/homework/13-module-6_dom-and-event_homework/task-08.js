444df0a59d7b063043eabaa13f1f126e3a546f75
Sergii79 <tsymbal.sergiy@gmail.com>
2024-01-31 08:38:27 +0100

    commit second

--- src/js/homework/13-module-6_dom-and-event_homework/task-08.js
+++ src/js/homework/13-module-6_dom-and-event_homework/task-08.js
@@ -1,1 +1,64 @@
+// const registerForm = document.querySelector('.login-form');
 
+// console.log(registerForm);
+
+// const handleSubmit = event => {
+//   event.preventDefault();
+
+//   const form = event.target;
+//   const email = form.elements.email.value;
+//   const password = form.elements.password.value;
+
+//   if (email === '' || password === '') {
+//     return alert('Please fill in all the fields!');
+//   }
+//   form.reset();
+// };
+
+// registerForm.addEventListener('submit', handleSubmit);
+
+//* 1-й варіант:
+
+// const form = document.querySelector('.login-form');
+// console.log(form);
+
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const {
+//     elements: { email, password },
+//   } = event.currentTarget;
+
+//   if (email.value === '' || password.value === '') {
+//     return alert('Please fill in all the fields!');
+//   }
+
+//   console.log(`email: ${email.value}, Password: ${password.value}`);
+//   event.currentTarget.reset();
+// }
+
+// form.addEventListener('submit', handleSubmit);
+
+//* 2-й варіант:
+
+const form = document.querySelector('.login-form');
+
+const user = {
+  email: '',
+  password: '',
+};
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === '' || password.value === '') {
+    return alert('All fields are required!');
+  }
+
+  user.email = email.value;
+  user.password = password.value;
+  console.log(user);
+  event.currentTarget.reset();
+});
