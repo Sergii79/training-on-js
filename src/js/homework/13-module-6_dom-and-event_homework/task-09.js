51da84c4625fd108cb30bece50e474e428648675
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-01 07:52:12 +0100

    commit second

--- src/js/homework/13-module-6_dom-and-event_homework/task-09.js
+++ src/js/homework/13-module-6_dom-and-event_homework/task-09.js
@@ -4,10 +4,25 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
 
+const colorElement = document.querySelector('body');
 const buttonElem = document.querySelector('.change-color');
+const colorNumber = document.querySelector('.color');
 
+//* 1-й варіант тіла функції:
 const backgroundChange = () => {
-  getRandomHexColor();
+  const changeColor = getRandomHexColor();
+  colorElement.style.backgroundColor = changeColor;
+  colorNumber.textContent = changeColor;
 };
+
+// function backgroundChange() {
+//   //* 2-й варіант тіла функції:
+//   colorElement.style.backgroundColor = getRandomHexColor();
+//   colorNumber.textContent = getRandomHexColor();
+//   //* 3-й варіант тіла функції:
+//   // const color = getRandomHexColor();
+//   // colorElement.style.backgroundColor = color;
+//   // colorNumber.textContent = color;
+// }
 
 buttonElem.addEventListener('click', backgroundChange);
