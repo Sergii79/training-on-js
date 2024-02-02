60cc5d9cdfdb2b8be11a5115fe4ba50d614f855f
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-02 08:52:26 +0100

    commit second

--- src/js/14-module-6_patterns-and-events_lesson-13.js
+++ src/js/14-module-6_patterns-and-events_lesson-13.js
@@ -12,8 +12,15 @@ function selectColor(event) {
   if (event.target.nodeName !== 'BUTTON') {
     return;
   }
+  // Обов'язково перевіряємо мету кліка, щоб це точно була кнопка, тому що ми не хочемо обробляти кліки в елемент-контейнері. Для перевірки типу елемента використовуємо властивість nodeName.
+  // Тут event.target є елементом, який викликав подію, і nodeName дозволяє вам отримати назву тегу цього елемента. Зауважте, що nodeName повертається у верхньому регістрі, наприклад, "BUTTON", "DIV", тощо.
 
-  const selectedColor = event.target.dataset.color;
+  const selectedColor = event.target.dataset.color; // Цей рядок коду призначений для отримання значення атрибута data-color елемента, на якому виникла подія (в даному випадку, клік миші).
+  // Давайте розглянемо його крок за кроком:
+  // event - це об'єкт події, який передається у функцію-обробник, яка викликається при виникненні події.
+  // event.target - це елемент, який викликав подію. Якщо, наприклад, подія виникла при кліку на кнопці, event.target буде посилатися саме на цю кнопку.
+  // .dataset.color - це спосіб отримати значення атрибута data-color елемента. Властивість dataset надає доступ до всіх атрибутів data-* елемента у зручній формі. В нашому випадку, .dataset.color повертає значення атрибута data-color.
+  // Отже, const selectedColor = event.target.dataset.color; зберігає в змінній selectedColor значення атрибута data-color елемента, на якому виникла подія.
   output.textContent = `Selected color: ${selectedColor}`;
   output.style.color = selectedColor;
 }
