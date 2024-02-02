afddd9943ff6d52d1b2e30e72e6f564d67a8c848
Sergii79 <110824315+Sergii79@users.noreply.github.com>
2024-02-02 06:26:08 +0000

    Deploying to gh-pages from @ Sergii79/training-on-js@cba7dab456461f8ae3d6935a66748f909c9577de 🚀

---
+++ сomponents/14-module-6_patterns-and-events_lesson-13.30f02593.js
@@ -0,0 +1,2 @@
+console.log("Hello!");
+//# sourceMappingURL=14-module-6_patterns-and-events_lesson-13.30f02593.js.map
---
+++ сomponents/15-module-6_patterns-and-events_lesson-14.13bfa071.js
@@ -0,0 +1,2 @@
+console.log("Hello!");
+//# sourceMappingURL=15-module-6_patterns-and-events_lesson-14.13bfa071.js.map
