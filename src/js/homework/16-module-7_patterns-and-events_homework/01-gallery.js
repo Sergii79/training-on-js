fb166c6f7371a3a9a91e7838f725bb84d230a369
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-10 09:36:26 +0100

    commit second

--- src/js/homework/16-module-7_patterns-and-events_homework/01-gallery.js
+++ src/js/homework/16-module-7_patterns-and-events_homework/01-gallery.js
@@ -1,14 +1,89 @@
+// // import * as basicLightbox from 'basiclightbox';
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+
+// console.log(galleryItems);
+
+// const containerGallery = document.querySelector('.gallery');
+
+// const createGallery = galleryItems.reduce((acum, item) => {
+//   return (acum += `<li><img src="${item.preview}" title="${item.description}"></li>`);
+// }, '');
+
+// containerGallery.insertAdjacentHTML('beforeend', createGallery);
+
+// //Библиотека модальных окон:
+// //https://www.jsdelivr.com/package/npm/basiclightbox?path=dist
+// //https://basiclightbox.electerious.com/
+
+// const createModalWindow = (original, description) => {
+//   return basicLightbox.create(
+//     `<img src="${original}" alt="${description}" width="800" height="600">`
+//   );
+// };
+
+// // createModalWindow.show();
+
+// // containerGallery.addEventListener('click', createModalWindow);
+
+// containerGallery.addEventListener('click', event => {
+//   event.preventDefault();
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+
+//   const imageSrc = event.target.getAttribute('src');
+//   const imageDescription = event.target.getAttribute('title');
+
+//   const modal = createModalWindow(imageSrc, imageDescription);
+//   modal.show();
+// });
+
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
 
-const containerGallery = document.querySelector('.gallery');
+// Обробка галереї в html
+const showGalleryItems = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" 
+    href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    data-source="${original}"
+    alt="${description}">
+    </img>
+    </a>
+    </div>`;
+  })
+  .join('');
 
-const createGallery = galleryItems.reduce((acum, item) => {
-  return (acum += `<li><img src="${item.preview}" title="${item.description}"></li>`);
-}, '');
+galleryBox.insertAdjacentHTML('afterbegin', showGalleryItems);
 
-containerGallery.insertAdjacentHTML('beforeend', createGallery);
+// Функція створення модального вікна
+function onGalleryContainerClick(event) {
+  event.preventDefault();
 
-// containerGallery.addEventListener('click', createGallery);
+  const isImg = event.target.classList.contains('gallery__image');
+  if (!isImg) {
+    return;
+  }
+
+  const originalLink = event.target.dataset.source;
+
+  showBigImg(originalLink);
+}
+
+galleryBox.addEventListener('click', onGalleryContainerClick);
+
+// Посилання на відкриття модального вікна https://basiclightbox.electerious.com
+function showBigImg(link) {
+  const instance = basicLightbox.create(`
+    <img src="${link}">
+`);
+
+  instance.show();
+}
