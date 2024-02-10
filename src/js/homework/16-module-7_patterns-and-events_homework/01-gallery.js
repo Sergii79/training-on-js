5dc7145ac435fd8ea4d769748e64090359503b6d
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-10 08:31:31 +0100

    commit second

--- src/js/homework/16-module-7_patterns-and-events_homework/01-gallery.js
+++ src/js/homework/16-module-7_patterns-and-events_homework/01-gallery.js
@@ -2,3 +2,13 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const containerGallery = document.querySelector('.gallery');
+
+const createGallery = galleryItems.reduce((acum, item) => {
+  return (acum += `<li><img src="${item.preview}" title="${item.description}"></li>`);
+}, '');
+
+containerGallery.insertAdjacentHTML('beforeend', createGallery);
+
+// containerGallery.addEventListener('click', createGallery);
