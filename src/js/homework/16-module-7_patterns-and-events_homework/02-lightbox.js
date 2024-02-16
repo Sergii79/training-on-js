b61b44b329549c9000908a4a110bba18c38c90b3
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-16 17:15:13 +0100

    commit second

--- src/js/homework/16-module-7_patterns-and-events_homework/02-lightbox.js
+++ src/js/homework/16-module-7_patterns-and-events_homework/02-lightbox.js
@@ -2,3 +2,22 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+console.log(galleryContainer);
+
+// Обробка галереї в html
+const galleryCreate = galleryItems.reduce(
+  (acum, { preview, original, description }) => {
+    return (acum += `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>`);
+  },
+  ''
+);
+
+galleryContainer.insertAdjacentHTML('afterbegin', galleryCreate);
+// galleryContainer.addEventListener('click', openGallery);
