dddaf85ee3aebe608532acb83c46886fc80edfc4
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-24 07:52:17 +0100

    commit second

--- src/server/index.js
+++ src/server/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const users = [
+  { name: 'Oleg', age: 15 },
+  { name: 'Sergey', age: 18 },
+  { name: 'Olya', age: 21 },
+];
+
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
 app.listen(port, () => {
