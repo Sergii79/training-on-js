f8e75cd4e00821276dec802e5db57e999daaaf32
Sergii79 <tsymbal.sergiy@gmail.com>
2024-02-27 12:30:11 +0100

    commit second

--- src/js/17-module-8_web-storage-infrastructure_lesson-15.js
+++ src/js/17-module-8_web-storage-infrastructure_lesson-15.js
@@ -1,67 +1,67 @@
 console.log('Hello!');
 
-import { v4 as uuidv4 } from 'uuid';
-import '../css/';
+// import { v4 as uuidv4 } from 'uuid';
+// import '../css/';
 
-const getTodo = ({ id, value, checked }) => `
-  <li data-id=${id}>
-    <input type="checkbox" ${checked ? 'checked' : ''} />
-    <span>${value}</span>
-    <button data-action="delete">x</button>
-    <button data-action="view">view</button>
-  </li>`;
+// const getTodo = ({ id, value, checked }) => `
+//   <li data-id=${id}>
+//     <input type="checkbox" ${checked ? 'checked' : ''} />
+//     <span>${value}</span>
+//     <button data-action="delete">x</button>
+//     <button data-action="view">view</button>
+//   </li>`;
 
-const refs = {
-  form: document.querySelector('.form'),
-  list: document.querySelector('.todo-list'),
-};
+// const refs = {
+//   form: document.querySelector('.form'),
+//   list: document.querySelector('.todo-list'),
+// };
 
-let todos = [];
+// let todos = [];
 
-const handleSubmit = event => {
-  const input = event.target.elements.text;
-  const { value } = input;
-  const newTodo = { id: uuidv4(), value, checked: false };
+// const handleSubmit = event => {
+//   const input = event.target.elements.text;
+//   const { value } = input;
+//   const newTodo = { id: uuidv4(), value, checked: false };
 
-  event.preventDefault();
-  todos.push(newTodo);
-  input.value = '';
-  render();
-};
+//   event.preventDefault();
+//   todos.push(newTodo);
+//   input.value = '';
+//   render();
+// };
 
-const deleteTodo = id => {
-  todos = todos.filter(todo => todo.id !== id);
-  render();
-};
+// const deleteTodo = id => {
+//   todos = todos.filter(todo => todo.id !== id);
+//   render();
+// };
 
-const viewTodo = id => {
-  console.log('view todo');
-};
+// const viewTodo = id => {
+//   console.log('view todo');
+// };
 
-const handleTodoClick = event => {
-  const { action } = event.target.dataset;
-  const parent = event.target.closest('li');
-  const { id } = parent?.dataset || {};
+// const handleTodoClick = event => {
+//   const { action } = event.target.dataset;
+//   const parent = event.target.closest('li');
+//   const { id } = parent?.dataset || {};
 
-  switch (action) {
-    case 'delete':
-      deleteTodo(id);
-      break;
+//   switch (action) {
+//     case 'delete':
+//       deleteTodo(id);
+//       break;
 
-    case 'view':
-      viewTodo(id);
-      break;
-  }
-};
+//     case 'view':
+//       viewTodo(id);
+//       break;
+//   }
+// };
 
-const render = () => {
-  const itemList = todos.map(todo => getTodo(todo)).join('');
+// const render = () => {
+//   const itemList = todos.map(todo => getTodo(todo)).join('');
 
-  refs.list.innerHTML = '';
-  refs.list.insertAdjacentHTML('beforeend', itemList);
-};
+//   refs.list.innerHTML = '';
+//   refs.list.insertAdjacentHTML('beforeend', itemList);
+// };
 
-render();
+// render();
 
-refs.form.addEventListener('submit', handleSubmit);
-refs.list.addEventListener('click', handleTodoClick);
+// refs.form.addEventListener('submit', handleSubmit);
+// refs.list.addEventListener('click', handleTodoClick);
